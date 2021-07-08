e566dc9343a0bf2e1715088ddde894d4e21b4533
Bentroen <bemcdc@gmail.com>
2021-07-08 17:14:15 -0300

    Add return type detection; write call result to shared buffer
    Code for writing to the buffer is from: https://forum.yoyogames.com/index.php?threads/return-ds-list-from-dll.26195/post-167078

--- pygml/pygml.cpp
+++ pygml/pygml.cpp
@@ -6,8 +6,35 @@ namespace py = pybind11;
 
 #define GMEXPORT extern "C" __declspec (dllexport)
 
+struct buffer {
+    char* pos;
+public:
+    buffer(char* origin) : pos(origin) {}
+    template<class T> T read() {
+        T r = *(T*)pos;
+        pos += sizeof(T);
+        return r;
+    }
+    template<class T> void write(T val) {
+        *(T*)pos = val;
+        pos += sizeof(T);
+    }
+    //
+    char* read_string() {
+        char* r = pos;
+        while (*pos != 0) pos++;
+        pos++;
+        return r;
+    }
+    void write_string(const char* s) {
+        for (int i = 0; s[i] != 0; i++) write<char>(s[i]);
+        write<char>(0);
+    }
+};
 
-GMEXPORT char* python_call(char* module, char* callable, char* args) {
+GMEXPORT double _python_run_file(char* cbuf, char* module, char* callable, char* args) {
+    // Set up buffer for writing result
+    buffer b(cbuf);
 
     // Initialize Python interpreter
     py::scoped_interpreter guard{};
@@ -25,18 +52,41 @@ GMEXPORT char* python_call(char* module, char* callable, char* args) {
 
         // Call line and return result as a string
         py::object result = py::eval(call);
-        std::string resultString = result.attr("__str__")().cast<std::string>();
-        char* cstr = new char[resultString.length() + 1];
-        strcpy_s(cstr, resultString.size() + 1, resultString.c_str());
-        return cstr;
 
+        // Buffer contains the result of the call
+        // Return code defines how the buffer value must be treated
+        if (result.is_none()) {
+            return 1;
+        }
+        else if (py::isinstance<py::bool_>(result)) {
+            int value = py::int_(result).cast<int>();
+            b.write<int>(value);
+            return 2;
+        }
+        else if (py::isinstance<py::int_>(result)) {
+            int value = result.cast<int>();
+            b.write<int>(value);
+            return 3;
+        }
+        else if (py::isinstance<py::float_>(result)) {
+            double value = result.cast<double>();
+            b.write<double>(value);
+            return 4;
+        }
+        else {
+            std::string value = py::str(result).cast<std::string>();
+            const char* cstr = value.c_str();
+            b.write_string(cstr);
+            return 100;
+        }
     }
     catch (py::error_already_set& e) {
         // Return exception and traceback as a string
         std::string exc = e.what();
-        char* cstr = new char[exc.length() + 1];
-        strcpy_s(cstr, exc.size() + 1, exc.c_str());
-        return cstr;
+        const char* cstr = exc.c_str();
+        std::cout << exc;
+        b.write(cstr);
+        return -1;
     }
 
     py::finalize_interpreter();
